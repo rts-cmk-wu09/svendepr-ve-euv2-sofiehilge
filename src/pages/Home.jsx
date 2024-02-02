d0d647da9f35262db0086f504477537c2d3ad1bc
sofiehilge <sofie.hilge@gmail.com>
2024-02-02 11:24:55 +0100

    fetch image to activitylist

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,9 @@
 import { Link } from "react-router-dom";
+import ActivityList from "../templates/ActivityList";
 const Home = () => {
     return ( <>
     <h3>Popular classes</h3>
+    <ActivityList/>
     <Link to="/classdetails"><p>Image</p>
     </Link>
     
---
+++ src/templates/ActivityList.jsx
@@ -0,0 +1,45 @@
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import LoadingComp from "../components/LoadingComp";
+
+const ActivityList = () => {
+  const [activityList, setActivityList] = useState();
+  console.log(activityList);
+
+  /* ---------Get all activities------ */
+  useEffect(() => {
+    const getActivityList = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+      },
+    };
+    fetch("http://localhost:4000/api/v1/classes", getActivityList)
+      .then((response) => response.json())
+      .then((response) => setActivityList(response))
+      .catch((err) => console.error(err));
+  }, []);
+  /* Loading statement */
+  if (!activityList || !activityList.length) {
+    return <LoadingComp />;
+  }
+
+  return (
+    <>
+      {activityList.map((item) => (
+        <Link key={item.id} to={`classdetails/${item.id}`}>
+          <article
+            className={"w-[128px] h-[144.87px]"}
+            style={{
+              backgroundImage: `url(${item.asset.url})`,
+              backgroundSize: "cover",
+            }}
+          ></article>
+        </Link>
+      ))}
+      <p>activityList</p>
+    </>
+  );
+};
+
+export default ActivityList;
