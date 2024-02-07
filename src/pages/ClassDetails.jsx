8489a7de63832ec1aa85fc81feb5426ad0ff1b08
sofiehilge <sofie.hilge@gmail.com>
2024-02-07 22:07:25 +0100

    add new sigunpbutton

--- src/pages/ClassDetails.jsx
+++ src/pages/ClassDetails.jsx
@@ -1,15 +1,17 @@
 import DetailsCard from "../templates/DetailsCard";
-import SignUpButtonFetch from "../components/SignUpButtonFetch";
+import SignUpButtonFetchMatch from "../components/SignUpButtonFetchMatch";
 
 const ClassDetails = () => {
   return (
     <>
       <DetailsCard />
       <div className="px-[20px]">
-        <SignUpButtonFetch />
+        <SignUpButtonFetchMatch />
       </div>
     </>
   );
 };
 
 export default ClassDetails;
+
+//I need to pass the weekday from detailsCard to signUpButtonFetchMatch and then compare in the sign up
